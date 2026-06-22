290b3dc5e43a48d18405b3ec7232e509a3f8beb7
leeyoungkon <yklee2002@gmail.com>
2026-06-22 15:49:37 +0900

    1st commit

---
+++ src/test/java/com/example/camit1/Camit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camit1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Camit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
